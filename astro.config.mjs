c8d13c702a73951bf47acaa65591e3c3888c5d07
Anna Hsu <annaddhsu@gmail.com>
2022-07-05 14:10:08 -0400

    add config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
-export default {
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
   vite: {
     ssr: {
       external: ['svgo']
     }
-  }
-}
+  },
+  integrations: [image()]
+});
