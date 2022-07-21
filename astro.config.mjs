90745143bfcffa0d64f85b3a1eccdc39a1e2263c
Anna Hsu <annaddhsu@gmail.com>
2022-07-21 12:58:42 -0400

    add robots.txt config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 
+import astro from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://annahsu.dev",
   vite: {
     ssr: {
-      external: ['svgo']
-    }
+      external: ["svgo"],
+    },
   },
-  integrations: [image()]
+  experimental: {
+    integrations: true,
+  },
+  integrations: [image(), astro()],
 });
