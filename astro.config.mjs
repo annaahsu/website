1559967ff8ba1216a91b0a39875130ab8cd3a188
Anna Hsu <annaddhsu@gmail.com>
2022-07-28 13:34:59 -0400

    update config for mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-
 import astro from "astro-robots-txt";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,8 +11,11 @@ export default defineConfig({
       external: ["svgo"],
     },
   },
+  markdown: {
+    syntaxHighlight: "prism",
+  },
   experimental: {
     integrations: true,
   },
-  integrations: [image(), astro()],
+  integrations: [image(), astro(), mdx()],
 });
